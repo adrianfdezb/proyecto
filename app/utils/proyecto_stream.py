ac650f25f36a17d6fc85ec7a802d0ae23a06741c
Adri FB <adrianfernandezblanco97@gmail.com>
2023-09-05 12:25:56 +0200

    Cambios 05.09

--- app/utils/proyecto_stream.py
+++ app/utils/proyecto_stream.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import altair as alt
+import requests
+import validators
 
 
 df = pd.read_csv('vehiculos-de-segunda-mano-sample.csv')
@@ -19,22 +21,31 @@ st.markdown('**¡Introduce los datos!**')
 image = Image.open("coches.jpeg")
 st.image(image, caption=None, width=400, output_format="JPEG")
 
-marca = st.selectbox('Elige una marca', marcas)
-
-for m in marcas:
-    if marca == m:
-        modelos = list(df.model[df['make'] == m].unique())
-        modelo = st.selectbox('Elige un modelo', modelos)
-
-      
-
-st.markdown(f'Vehículo elegido: {marca} {modelo}')
+marca = st.selectbox('Elige una marca', marcas)     
 km = st.slider('¿Cuántos kms tiene?', 0, 650000, 5000)
 edad = st.slider('¿Cuántos años tiene?', 0, 60, 5)
 fuel = st.selectbox('Tipo de combustible', list(df['fuel'].dropna().unique()))
 shift = st.selectbox('Tipo de cambio', list(df['shift'].dropna().unique()))
+power = st.slider('¿Cuántos caballos tiene?', 60, 200, 110)
+st.markdown(f'Vehículo elegido: {marca}, Kms: {km}, Antigüedad: {edad}, Combustible: {fuel}, Cambio: {shift}, Caballos: {power}')
+
+from sklearn import linear_model
+
+def entrenar_modelo():
+    reg = linear_model.Lasso(alpha=0.1)
+    reg.fit([[0, 23], [2323, 9999]], [0, 345])
+    return reg
+
+entrenado = entrenar_modelo()
+
+def prediccion(x, y):
+    pred = entrenado.predict([[x, y]])
+    return pred
 
 
-
-
-
+st.title(f'Precio óptimo al que puedes anunciar tu coche')
+#st.button("Reset", type="primary")
+if st.button('Calcular:'):
+    st.write(f'Predicción: {prediccion(power,edad)}')
+else:
+    st.write(' ')
