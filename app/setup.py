52111eb3657179b17ca0e6626fff08db9fff4c6e
adrianfdezb <139003702+adrianfdezb@users.noreply.github.com>
2023-09-28 12:23:13 +0200

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -1,6 +1,19 @@
 from setuptools import setup, find_packages
 
 
+requirements = [
+    'streamlit',
+    'scikit-learn==1.3.0',
+    'pickleshare==0.7.5',
+    'pandas',
+    'numpy',
+    'altair',
+    'requests',
+    'validators',
+    'Pillow==8.4.0',
+    'Modelo_XGBoostRegresion'
+]
+
 setup(
     name='proyecto',
     version='0.1',
