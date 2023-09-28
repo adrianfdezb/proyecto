347dc703a2968563efc57ad97b54243c098c686e
adrianfdezb <139003702+adrianfdezb@users.noreply.github.com>
2023-09-28 11:47:13 +0200

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -5,15 +5,6 @@ setup(
     name='proyecto',
     version='0.1',
     packages=find_packages(),
-    install_requires=[streamlit,
-                      scikit-learn==1.3.0,
-                      pickleshare==0.7.5,
-                      pandas,
-                      numpy,
-                      altair,
-                      requests,
-                      validators,
-                      Pillow==8.4.0,
-                      git+https://github.com/adrianfdezb/proyecto.git@main#egg=proyecto&subdirectory=app/utils
+    install_requires=requirements
     ]
 )
